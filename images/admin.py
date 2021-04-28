a170d9106e8772261062b3e95e9cfa5781d0b78a
Kieryn Davison <kmdaviso@uwaterloo.ca>
2021-04-28 17:21:37 -0400

    Customize the admin display a bit

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Image
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image_view', 'desc', 'price_view', 'offer')
+
+admin.site.register(Image, ImageAdmin)
--- images/models.py
+++ images/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.utils.html import mark_safe
+
 
 # Create your models here.
 
-class Images(models.Model):
+class Image(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='pics')
     desc = models.TextField()
     price = models.IntegerField()
     offer = models.BooleanField(default=False)
+
+    def image_view(self):
+        if self.image:
+            return mark_safe('<img src="{0}" width="100" height="100" style="object-fit:contain" />'.format(self.image.url))
+        else:
+            return 'No image found'
+
+    image_view.short_description = 'Image preview'
+
+    def price_view(self):
+        return '%.2f' % self.price
+
+    price_view.short_description = "Price"
